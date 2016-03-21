d27db8e9d04586e3d16ff4374ee55f938d8bb02b
Sergio <sergiomattei@outlook.com>
2016-03-21 14:55:45 -0400

    Created StatusController

---
+++ app/Http/Controllers/v1/StatusController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+use App\Http\Controllers\Controller;
+
+class StatusController extends Controller
+{
+    function ping() {
+    	return ['pong' => true, 'success' => true];
+    }
+}

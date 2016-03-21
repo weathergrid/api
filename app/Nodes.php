613d1daa7841c6105e31e1463ad2c1dc93379c5b
Sergio <sergiomattei@outlook.com>
2016-03-21 17:40:03 -0400

    Create nodes model

---
+++ app/Nodes.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Nodes extends Model
+{
+    /**
+     * The nodes table.
+     *
+     * @var string
+     */
+    protected $table = 'nodes';
+}

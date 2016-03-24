ad8c05124bc691e8e7ed471663c5c837eb9110ea
Sergio <sergiomattei@outlook.com>
2016-03-24 15:11:56 -0400

    Tests

--- tests/v1/NodesListTest.php
+++ tests/v1/NodesListTest.php
@@ -2,24 +2,41 @@
 
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class NodesListTest extends TestCase
-{
+class NodesListTest extends TestCase {
+
     /**
-     * Test /nodes/list page.
+     * Test nodes/list/all page.
      * 
      * @return void
      */
-    function testList() {
+    function testAll()
+    {
         // Test first seed result.
         $arr = $this->get('v1/nodes/list/all')->seeJson([
-            'success' => true,
             'ip' => '127.0.0.1'
         ]);
         // Test first offset seed result.
         $arr = $this->get('v1/nodes/list/all', ['offset' => 1])->seeJson([
-            'success' => true,
             'ip' => '8.8.8.8'
         ]);
         $this->assertResponseOk();
     }
+
+    /**
+     * Test nodes/list/region search.
+     * @return void
+     */
+    function testRegion() 
+    {
+        // IP registered with Puerto Rico region is localhost.
+        $arr = $this->get('v1/nodes/list/region/Puerto_Rico')->seeJson([
+            'ip' => '127.0.0.1'
+        ]);
+
+        // Listed Google's PDNS IP :D
+        $arr = $this->get('v1/nodes/list/region/California')->seeJson([
+            'ip' => '8.8.8.8'
+        ]);
+    }
+    
 }
